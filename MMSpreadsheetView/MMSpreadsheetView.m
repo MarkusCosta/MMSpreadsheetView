1f7c951a8b399c55af8cdee2f529e3128e8e2a23
Marcus Costa <marcus.costa@redspark.io>
2016-06-22 11:56:05 -0300

    fix scroll to front

--- MMSpreadsheetView/MMSpreadsheetView.m
+++ MMSpreadsheetView/MMSpreadsheetView.m
@@ -965,14 +965,12 @@ const static NSUInteger MMScrollIndicatorTag = 12345;
 
 - (void)scrollToFrontAnimated:(BOOL) animated {
     if(self.upperRightCollectionView.contentSize.width >= self.upperRightContainerView.frame.size.width) {
-        [UIView animateWithDuration:0.3 animations:^{
-    //        [self.upperLeftCollectionView setContentOffset:CGPointMake(self.upperLeftCollectionView.contentSize.width, self.upperLeftCollectionView.contentOffset.y) animated:animated];
-            [self.upperRightCollectionView setContentOffset:CGPointMake(self.upperRightCollectionView.contentSize.width, self.upperRightCollectionView.contentOffset.y) animated:animated];
-    //        [self.lowerLeftCollectionView setContentOffset:CGPointMake(self.lowerLeftCollectionView.contentSize.width, self.lowerLeftCollectionView.contentOffset.y) animated:animated];
-            [self.lowerRightCollectionView setContentOffset:CGPointMake(self.lowerRightCollectionView.contentSize.width, self.lowerRightCollectionView.contentOffset.y) animated:animated];
-            [self invalidateIntrinsicContentSize];
-//            [self layoutIfNeeded];
-        }];
+        [self.upperRightCollectionView setContentOffset:CGPointMake(self.upperRightCollectionView.contentSize.width, self.upperRightCollectionView.contentOffset.y) animated:animated];
+        [self.lowerRightCollectionView setContentOffset:CGPointMake(self.lowerRightCollectionView.contentSize.width, self.lowerRightCollectionView.contentOffset.y) animated:animated];
+//        [UIView animateWithDuration:0.3 animations:^{
+//            [self.upperRightCollectionView setContentOffset:CGPointMake(self.upperRightCollectionView.contentSize.width, self.upperRightCollectionView.contentOffset.y) animated:animated];
+//            [self.lowerRightCollectionView setContentOffset:CGPointMake(self.lowerRightCollectionView.contentSize.width, self.lowerRightCollectionView.contentOffset.y) animated:animated];
+//        }];
     }
 }
 
